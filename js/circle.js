70153c1857cb157f419eb09945966e9db40add0e
Gaspard89 <kasper.slusarczyk@gmail.com>
2015-10-11 16:36:50 +0200

    no message

--- js/circle.js
+++ js/circle.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         var child = $(this)[0].children[0];
         var word = $(this)[0].children[1];
         var hidden = $(this)[0].children[2];
-        $(child).fadeOut(300);
+        //$(child).fadeOut(300);
         $(word).fadeOut(300);
         $(hidden).fadeIn(300);
         })
@@ -15,7 +15,7 @@ $(document).ready(function(){
         var child = $(this)[0].children[0];
         var word = $(this)[0].children[1];
         var hidden = $(this)[0].children[2];
-        $(child).fadeIn(300);
+        //$(child).fadeIn(300);
         $(word).fadeIn(300);
         $(hidden).fadeOut(300);
     })
